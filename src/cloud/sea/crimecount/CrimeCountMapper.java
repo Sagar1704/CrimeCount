f893a80f456bdf265bd60ac3dccab2c615b7b8cf
Sagar Deshpande <deshpandesagar1704@gmail.com>
2015-11-05 19:14:19 -0600

    Done with map reduce. Testing remaining.

--- src/cloud/sea/crimecount/CrimeCountMapper.java
+++ src/cloud/sea/crimecount/CrimeCountMapper.java
@@ -1,5 +1,47 @@
 package cloud.sea.crimecount;
 
-public class CrimeCountMapper {
+import java.io.IOException;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class CrimeCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+	private final static IntWritable one = new IntWritable(1);
+	private Text regionCrime = new Text();
+
+	@Override
+	public void map(Object key, Text value, Context output) throws IOException, InterruptedException {
+		String crimeLine = value.toString();
+
+		String[] crimeData = crimeLine.split(",");
+		String easting = "";
+		if (crimeData.length > 4)
+			easting = getRegion(crimeData[4]);
+
+		String northing = "";
+		if (crimeData.length > 5)
+			northing = getRegion(crimeData[5]);
+
+		String crime = "";
+		if (crimeData.length > 7) {
+			crime = crimeData[7];
+
+			regionCrime.set(easting + "," + northing + "," + crime);
+
+			output.write(regionCrime, one);
+		}
+	}
+
+	private String getRegion(String region) {
+		String temp = "";
+
+		if (region != null && !region.isEmpty()) {
+			temp = region.substring(0, 1);
+
+			for (int i = 0; i < region.length() - 1; i++)
+				temp += "x";
+		}
+		return temp;
+	}
 }
--- src/cloud/sea/crimecount/CrimeCountReducer.java
+++ src/cloud/sea/crimecount/CrimeCountReducer.java
@@ -1,5 +1,19 @@
 package cloud.sea.crimecount;
 
-public class CrimeCountReducer {
+import java.io.IOException;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class CrimeCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context output)
+			throws IOException, InterruptedException {
+		int crimeCount = 0;
+		for (IntWritable value : values) {
+			crimeCount += value.get();
+		}
+		output.write(key, new IntWritable(crimeCount));
+	}
 }
